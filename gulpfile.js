f17ef8dc0962984cf2dbe7372e49a948d98cb08c
leo <leo@users.noreply.github.com>
2015-11-01 21:05:19 +0100

    Fully automate tasks
    No need to add pre-built vendor files to the module anymore.

--- gulpfile.js
+++ gulpfile.js
@@ -2,68 +2,42 @@ var gulp = require( 'gulp' ),
 	sourcemaps = require( 'gulp-sourcemaps' ),
 	del = require( 'del' ),
 	uglify = require( 'gulp-uglify' ),
-	intercept = require( 'gulp-intercept' ),
-	babel = require( 'babel-core' ),
-	fs = require( 'fs' ),
-	compiler = require( 'ember-compiler' );
+	compiler = require( 'gulp-ember-compiler' ),
+	concat = require( 'gulp-concat' ),
+	download = require( 'gulp-download' ),
+	jquery = require( 'gulp-jquery' );
 
 var paths = {
-	scripts: [ 'assets/app.js' ],
-	vectors: [ 'assets/vecotrs/*.svg' ],
-	templates: [ 'templates/*.hbs' ]
+	vectors: [ 'assets/vectors/*.svg' ],
+	app: [ 'templates/*.hbs', 'assets/app.js' ]
 };
 
 gulp.task( 'clean', function( cb ) {
 	del( ['build'], cb );
 });
 
-var loadTemplates = function() {
+gulp.task( 'vendor', function() {
 
-	var base = '',
-		templates = fs.readdirSync( 'templates' );
-
-	var template = compiler.precompile( '{{outlet}}', false );
-	base += 'Ember.TEMPLATES["application"] = Ember.HTMLBars.template(' + template + ');';
-
-	for( id in templates ) {
-
-		var file = templates[id],
-			input = fs.readFileSync( 'templates/' + file, { encoding: 'utf8' } ),
-			template = compiler.precompile( input, false );
-
-		base += 'Ember.TEMPLATES["' + file.split( '.' )[0] + '"] = Ember.HTMLBars.template(' + template + ');';
-
-	}
-
-	return base;
-
-}
-
-var transpile = function( file ) {
-
-	var content = loadTemplates(),
-		old = file.contents.toString( 'utf-8' ),
-		transpile = babel.transform( content, {
-			presets: [ 'es2015' ]
-		});
-
-	file.contents = new Buffer( transpile.code + old );
-	return file;
-
-}
-
-gulp.task( 'templates', function() {
-	return gulp.src( paths.templates )
-});
-
-gulp.task( 'app', function() {
-
-	return gulp.src( paths.scripts )
-	//.pipe( sourcemaps.init() )
-	//.pipe( uglify() )
-	//.pipe( sourcemaps.write() )
+	return jquery.src()
+	.pipe( download( 'http://builds.emberjs.com/release/ember.prod.js' ) )
+	.pipe( concat( 'vendor.js' ) )
+	.pipe( sourcemaps.init() )
+	.pipe( uglify() )
+	.pipe( sourcemaps.write( '/' ) )
 	.pipe( gulp.dest( 'build' ) );
 
 });
 
-gulp.task( 'default', [ 'app' ] );
+gulp.task( 'app', function() {
+
+	return gulp.src( paths.app )
+	.pipe( compiler() )
+	.pipe( concat( 'app.js' ) )
+	.pipe( sourcemaps.init() )
+	.pipe( uglify() )
+	.pipe( sourcemaps.write( '/' ) )
+	.pipe( gulp.dest( 'build' ) );
+
+});
+
+gulp.task( 'default', [ 'clean', 'vendor', 'app' ] );
