029664af79421315e49ad2a13ac242c86e630778
leo <leo@users.noreply.github.com>
2015-11-10 20:57:28 +0100

    No includePath needed

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,17 +3,18 @@ var EmberApp = require( 'ember-cli/lib/broccoli/ember-app' );
 module.exports = function( defaults ) {
 
 	var app = new EmberApp( defaults, {
-		sassOptions: {
-			includePaths: [ './app/styles' ]
-		},
+
 		outputPaths: {
+
 			app: {
 				js: '/assets/app.js',
 				css: {
 					'app': '/assets/styles.css'
 				}
 			}
+
 		}
+
 	});
 
 	// Use `app.import` to add additional libraries to the generated output files.
