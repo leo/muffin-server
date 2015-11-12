2378fcdf15e7ae7356b6758148f510259ffc7089
leo <leo@users.noreply.github.com>
2015-11-12 17:26:45 +0100

    Adjusted testing environment

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
-export default function startApp(attrs) {
+export default function startApp( attrs ) {
 
-	var application;
+	let application;
 
-	var attributes = Ember.merge( {}, config.APP );
+	let attributes = Ember.merge( {}, config.APP );
 	attributes = Ember.merge( attributes, attrs );
 
-	Ember.run( function() {
+	Ember.run(() => {
 		application = Application.create( attributes );
 		application.setupForTesting();
 		application.injectTestHelpers();
