b04697aa0df91e104a53684ce9cd9a6db74c856d
leo <leo@users.noreply.github.com>
2015-11-13 22:21:08 +0100

    Changed development workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require( 'gulp' ),
-	shell = require( 'gulp-shell' );
+	shell = require( 'gulp-shell' ),
+	nodemon = require( 'gulp-nodemon' );
+
+gulp.task( 'core', function() {
+
+	nodemon({
+		script: 'index.js',
+		ext: 'js json',
+		ignore: [ 'client' ]
+	});
+
+});
+
+gulp.task( 'watch', [ 'core' ], shell.task([
+	'ember build --watch',
+], {
+	cwd: process.cwd() + '/client'
+}));
 
 gulp.task( 'default', shell.task([
 	'npm install',
