63ec120c596cebc8110e4c9e83523d4e59f9f970
leo <leo@users.noreply.github.com>
2015-11-15 18:57:35 +0100

    Oh, hi dashboard 😘

---
+++ app/routes/dashboard.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+	renderTemplate() {
+		this.render( 'admin', {
+			controller: 'dashboard'
+		});
+	}
+
+});
--- tests/unit/routes/dashboard-test.js
+++ tests/unit/routes/dashboard-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:index', 'Unit | Route | index', {
+moduleFor('route:dashboard', 'Unit | Route | dashboard', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
