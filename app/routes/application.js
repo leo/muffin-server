1397ac1358fca832deeff41d10dbe17efcc76d1a
leo <leo@users.noreply.github.com>
2015-11-15 19:50:28 +0100

    Automatic body class

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend( ApplicationRouteMixin );
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import BodyClassMixin from 'ember-body-class/mixins/body-class';
+
+export default Ember.Route.extend( ApplicationRouteMixin, BodyClassMixin, {} );
--- app/routes/login.js
+++ app/routes/login.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 	renderTemplate() {
-		$( 'body' ).addClass( 'login' );
 		this.render( 'login' );
 	}
 
