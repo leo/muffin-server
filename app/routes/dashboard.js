a8321824ba18e1655167b216d3c03748673cb1e9
leo <leo@users.noreply.github.com>
2015-11-15 19:28:52 +0100

    Remove "login" class after transition

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+	activate() {
+		$( 'body' ).removeClass( 'login' )
+	},
+
 	renderTemplate() {
 		this.render( 'admin', {
 			controller: 'dashboard'
