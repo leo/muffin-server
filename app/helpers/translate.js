293439578618c0c014db5195bfb60553e8155992
leo <leo@users.noreply.github.com>
2015-11-20 17:26:40 +0100

    Translation helper added

---
+++ app/helpers/translate.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function translate( params/*, hash*/ ) {
+	return params;
+}
+
+export default Ember.Helper.helper( translate );
---
+++ tests/unit/helpers/translate-test.js
@@ -0,0 +1,10 @@
+import { translate } from '../../../helpers/translate';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | translate');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = translate(42);
+  assert.ok(result);
+});
