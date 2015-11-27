cce0006a9a369b8c3da784be3f4ab2073b563d36
leo <leo@users.noreply.github.com>
2015-11-27 19:26:12 +0100

    Don't leave so much space 💤

--- app/components/toggle-nav.js
+++ app/components/toggle-nav.js
@@ -8,14 +8,12 @@ export default Ember.Component.extend({
 	href: '#',
 
 	click: function( event ) {
-
 		var button = $( event.target ).closest( '.toggle' );
 
 		$( button ).prevAll( 'nav' ).slideToggle( 300 );
 		$( button ).toggleClass( 'on' );
 
 		event.preventDefault();
-
 	}
 
 });
