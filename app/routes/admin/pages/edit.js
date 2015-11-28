f82c6cb76ae78c0dd7ebadccd52af010b4e8e6d7
leo <leo@users.noreply.github.com>
2015-11-28 13:53:02 +0100

    Show title of page in detail view

--- app/routes/admin/pages/edit.js
+++ app/routes/admin/pages/edit.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	titleToken: function( model ) {
+		return model.get( 'title' );
+	},
+	model: function( params ) {
+		return this.store.findRecord( 'page', params.id );
+	}
 });
