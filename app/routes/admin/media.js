ba32f3389d4f86ba5fdbdf943a5d66f0306dc530
leo <leo@users.noreply.github.com>
2015-11-30 21:01:18 +0100

    Delete empty lines

--- app/routes/admin/media.js
+++ app/routes/admin/media.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
 	titleToken: 'Media',
-
 	model() {
 		return this.store.findAll( 'file' );
 	}
-
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -4,9 +4,7 @@ import SimpleAuth from 'ember-simple-auth/mixins/application-route-mixin';
 import BodyClass from 'ember-body-class/mixins/body-class';
 
 export default Ember.Route.extend( SimpleAuth, BodyClass, {
-
 	title: function( tokens ) {
 		return tokens.join( ' ― ' ) + ' ― Muffin';
 	}
-
 });
