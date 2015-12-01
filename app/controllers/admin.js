e6d7b4e9c5267ea2f992d9db4b027bd4e25d8e9d
leo <leo@users.noreply.github.com>
2015-12-01 15:33:49 +0100

    Don't always show tabs

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	title: 'I\'m the title',
+	tabs: true,
 	actions: {
 		openModal: function( target ) {
 			var modal = this.get( 'modal-' + target );
