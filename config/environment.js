e98ab5e27a6f1cb94b20bc8a48e640be5776cbc5
leo <leo@users.noreply.github.com>
2015-12-02 11:46:34 +0100

    Automatically inject version

--- config/environment.js
+++ config/environment.js
@@ -16,8 +16,7 @@ module.exports = function( environment ) {
 		},
 
 		APP: {
-			// Here you can pass flags/options to your application instance
-			// when it is created
+			version: process.env.npm_package_version
 		},
 
 		contentSecurityPolicy: {
