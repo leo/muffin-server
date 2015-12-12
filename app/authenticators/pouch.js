206c37b79b61c9fd8e3da62a7e1b911f0274c492
leo <leo@users.noreply.github.com>
2015-12-12 18:45:33 +0100

    Fixed JSHint errors

--- app/authenticators/pouch.js
+++ app/authenticators/pouch.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 
-const { RSVP, isEmpty, run } = Ember;
+const { RSVP } = Ember;
 
 export default Base.extend({
 
   restore(data) {
-
+    console.log(data);
   },
 
-  authenticate(identification, password, scope = []) {
+  authenticate(identification, password) {
 
     return new RSVP.Promise((resolve, reject) => {
 
@@ -23,8 +23,8 @@ export default Base.extend({
 
   },
 
-  invalidate( data ) {
-
+  invalidate(data) {
+    console.log(data);
   }
 
 });
