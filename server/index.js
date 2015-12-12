e28e1933540c5b451c327dbb63ac71df322bda6a
leo <leo@users.noreply.github.com>
2015-12-12 18:46:00 +0100

    The db connection doesn't need to be closed

--- server/index.js
+++ server/index.js
@@ -7,14 +7,6 @@ module.exports = function( app, options ) {
 
   app.use( compression() );
 
-  process.on( 'SIGINT', function() {
-
-    db.close( function() {
-      process.exit();
-    });
-
-  });
-
   app.get( '/muffin', function( req, res, next ) {
 
     if( req.path != '/muffin/' ) {
