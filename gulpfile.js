b8a83fb6fdb86d5b1199dfd0947bc5c8f5d76ac0
leo <leo@users.noreply.github.com>
2015-12-21 20:21:54 +0100

    Even more advanced naming :sunglasses:

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 
 gulp.task('scss', () => {
-  return gulp.src('client/styles/*.scss')
+  return gulp.src('client/style/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed',
-      includePaths: ['client/styles']
+      includePaths: ['client/style']
     }).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', () => {
-  return gulp.src('client/src/*.js')
+  return gulp.src('client/source/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
@@ -23,8 +23,8 @@ gulp.task('js', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch('client/styles/*.scss', ['scss']);
-  gulp.watch('client/src/*.js', ['js']);
+  gulp.watch('client/style/*.scss', ['scss']);
+  gulp.watch('client/source/*.js', ['js']);
 });
 
 gulp.task('default', ['scss', 'js', 'watch'] );
