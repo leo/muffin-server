3aa2e924f29ade8dfcd51c8ab24761ad10bcc4dc
Leonard Lamprecht <mindrun@icloud.com>
2015-12-24 16:26:28 +0100

    Serve first page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
 gulp.task('scss', () => {
   return gulp.src('client/style/*.scss')
@@ -11,6 +12,7 @@ gulp.task('scss', () => {
       includePaths: ['client/style']
     }).on('error', sass.logError))
     .pipe(sourcemaps.write())
+    .pipe(concat('styles.css'))
     .pipe(gulp.dest('dist'));
 });
 
@@ -19,12 +21,19 @@ gulp.task('js', () => {
     .pipe(babel({
       presets: ['es2015']
     }))
+    .pipe(concat('app.js'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('vectors', () => {
+  return gulp.src('client/vectors/*')
+  .pipe(gulp.dest('dist/vectors'));
 });
 
 gulp.task('watch', () => {
   gulp.watch('client/style/*.scss', ['scss']);
   gulp.watch('client/source/*.js', ['js']);
+  gulp.watch('client/assets/*', ['vectors']);
 });
 
-gulp.task('default', ['scss', 'js', 'watch'] );
+gulp.task('default', ['scss', 'js', 'vectors', 'watch'] );
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const nano = require('nano')('http://localhost:5984');
+var express = require('express'),
+    app = express(),
+    nano = require('nano')('http://localhost:5984'),
+    handlebars = require('handlebars'),
+    fs = require('fs')
 
 nano.db.create('muffin', function(err, body) {
 
@@ -8,4 +12,91 @@ nano.db.create('muffin', function(err, body) {
 
   var db = nano.use('muffin');
 
+  nano.auth('ABBA', 'dancing-queen', function(err, body, headers) {
+
+    var cookies = {};
+
+    if (err) {
+      return console.log(err);
+    }
+
+    if (headers && headers['set-cookie']) {
+      cookies['ABBAs'] = headers['set-cookie'];
+    }
+
+    console.log(null, 'You\'re an admin!');
+  });
+
+});
+
+app.use( '/admin/assets', express.static('dist') );
+
+function loadView(view, err, content) {
+
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  var look = fs.readFileSync('server/views/' + view + '.hbs', 'utf8'),
+    template = handlebars.compile(look);
+
+  if (view !== 'edit') {
+    var other = {
+      items: content
+    }
+  }
+
+  var tags = {
+    pageTitle: function() {
+      switch(view) {
+        case 'dashboard':
+          return 'Dashboard';
+
+        case 'edit':
+          return content.title;
+
+        case 'list':
+          return 'Pages';
+
+        default:
+          return 'Error 404';
+      }
+    },
+    content: template(view == 'edit' ? content : other),
+    view: view
+  }
+
+  var base = fs.readFileSync('server/templates/admin.hbs', 'utf8'),
+    container = handlebars.compile(base);
+
+  return container(tags);
+}
+
+app.get( '/admin', function(req, res) {
+
+  var view;
+
+  if (false) {
+    view = loadView('dashboard');
+  } else {
+    var base = fs.readFileSync('server/templates/login.hbs', 'utf8'),
+        template = handlebars.compile(base);
+
+    var tags = {
+      site: {
+        name: 'Volkspark'
+      }
+    }
+
+    view = template(tags);
+  }
+
+  res.send(view);
+
+});
+
+app.listen(3000, function() {
+  var port = this.address().port;
+  console.log('Muffin is running at http://localhost:' + port + '/admin');
 });
