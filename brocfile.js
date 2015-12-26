e997c7b5cab4f7707ca3928201bb935e0c0b9665
Leonard Lamprecht <mindrun@icloud.com>
2015-12-26 11:32:29 +0100

    Copy static files, too

--- brocfile.js
+++ brocfile.js
@@ -2,19 +2,24 @@
 var compileSass = require('broccoli-sass');
 var esTranspiler = require('broccoli-babel-transpiler');
 var mergeTrees = require('broccoli-merge-trees');
+var funnel = require('broccoli-funnel');
 
 // Specify the source directories
 var sassDir = 'public/styles';
-var babelDir = 'public/scripts';
+var jsDir = 'public/scripts';
 
 // Tell Broccoli how we want the assets to be compiled
-var styles = compileSass([sassDir], 'admin.scss', 'styles.css', {
+var styles = compileSass([sassDir], 'app.scss', 'styles.css', {
   outputStyle: 'compressed'
 });
 
-var scripts = esTranspiler(babelDir, {
+var scripts = esTranspiler(jsDir, {
   compact: true
 });
 
+var staticFiles = funnel('public', {
+  exclude: ['scripts/*', 'styles/*']
+});
+
 // Merge the compiled styles and scripts into one output directory.
-module.exports = mergeTrees([styles, scripts]);
+module.exports = mergeTrees([styles, scripts, staticFiles]);
