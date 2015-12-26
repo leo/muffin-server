1a335ad75c0d9e9f3de9fc6f2bc2f0126317569b
Leonard Lamprecht <mindrun@icloud.com>
2015-12-26 14:45:07 +0100

    Switch back to gulp
    Broccoli is weird for server-side running apps

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+const dirs = {
+  sass: 'public/styles/*.scss',
+  js: 'public/scripts/*.js',
+  vectors: 'public/vectors/*'
+}
+
+gulp.task('styles', () => {
+  return gulp.src(dirs.sass)
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: ['client/style']
+    }).on('error', sass.logError))
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src(dirs.js)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('vectors', () => {
+  return gulp.src(dirs.vectors)
+  .pipe(gulp.dest('dist/vectors'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(dirs.sass, ['styles']);
+  gulp.watch(dirs.js, ['scripts']);
+  gulp.watch(dirs.vectors, ['vectors']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'vectors', 'watch'] );
