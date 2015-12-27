70321ae11ab98296dafac97221bc2897ce41c5e7
Leonard Lamprecht <mindrun@icloud.com>
2015-12-27 14:07:48 +0100

    Delete models

--- app/models/page.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  title: DS.attr( 'string' ),
-  author: DS.attr( 'string' ),
-  rev: DS.attr( 'string' )
-});
--- app/models/user.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  password: DS.attr( 'string' ),
-  rev: DS.attr( 'string' )
-});
