d41feaa6b49cbf5ed84a6f87bd3e4b67b0e4dad7
Leonard Lamprecht <mindrun@icloud.com>
2015-12-31 20:40:53 +0100

    Load pages from DB

--- routes/pages.js
+++ routes/pages.js
@@ -1,17 +1,34 @@
 const express = require('express'),
-      router = express.Router();
+      router = express.Router(),
+      nano = require('nano')('http://localhost:5984'),
+      db = nano.use('muffin');
 
 router.get('/', function(req, res) {
 
-  res.render('list', {
-    pageTitle: 'Pages',
-    path: req.originalUrl
+  db.list({ type: 'page', include_docs: true }, function(err, body) {
+
+    if (err) {
+      return console.log(err);
+    }
+
+    var list = [];
+
+    body.rows.forEach(function(page) {
+      list.push(page.doc)
+    });
+
+    res.render('list', {
+      pageTitle: 'Pages',
+      path: req.originalUrl,
+      items: list
+    });
+
   });
 
 });
 
 router.get('/:id', function(req, res) {
-  
+
   res.render('edit', {
     pageTitle: 'LOL - what?',
     editableTitle: true,
