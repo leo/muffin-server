624b34875cb88538382bea8f95c07dfb8177497b
Leonard Lamprecht <mindrun@icloud.com>
2016-01-05 12:08:45 +0100

    Make menu toggle in vanilla JS

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,7 +1,20 @@
-/*
-$('#chest .toggle').click(function(event) {
-  $(this).prevAll('nav').slideToggle(300);
-  $(this).toggleClass('on');
+(function() {
 
-  event.preventDefault();
-});*/
+  if (document.body.classList.contains('login')) {
+    return;
+  }
+
+  const chest = document.querySelector('#chest'),
+        toggler = chest.querySelector('.toggle'),
+        nav = chest.querySelector('nav');
+
+  chest.querySelector('.toggle').addEventListener('click', function(event) {
+
+    nav.classList.toggle('open');
+    this.classList.toggle('on');
+
+    event.preventDefault();
+
+  });
+
+})();
