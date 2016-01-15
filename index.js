467d392f15ea16206d49595393d7e4e7c145cd46
Leonard Lamprecht <mindrun@icloud.com>
2016-01-15 16:53:59 +0100

    Use access data from env file for DB connection

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const app = express()
 const handlebars = require('express-handlebars')
 const session = require('express-session')
 
-const compression = require('compression')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
-const rope = require('./lib/db').rope
-const open = require('open')
+require('dotenv').config({
+  path: './.env'
+})
 
 const MongoStore = require('connect-mongo')(session)
 const busboy = require('connect-busboy')
 const livereload = require('connect-livereload')
 
-require('dotenv').config({
-  path: './.env'
-})
+const logger = require('morgan')
+const rope = require('./lib/db').rope
+const open = require('open')
+const compression = require('compression')
+const bodyParser = require('body-parser')
 
 process.on('SIGINT', function () {
   rope.close(function () {
