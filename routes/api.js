6cce84efed75dbb28e669d8a401ec0f56a356139
Leo <leo@users.noreply.github.com>
2016-03-02 17:41:21 +0100

    Simplify routes

--- routes/api.js
+++ routes/api.js
@@ -108,33 +108,25 @@ router.post('/token-refresh', function *(next) {
   yield next
 })
 
-router.get('/pages', function *(next) {
+function singular (type) {
+  return type.substring(0, type.length - 1)
+}
+
+router.get('/:type', function *(next) {
+  const type = this.params.type
+
   this.body = {
-    data: yield load.all('page')
+    data: yield load.all(singular(type))
   }
 
   yield next
 })
 
-router.get('/pages/:id', function *(next) {
+router.get('/(.*)/:id', function *(next) {
+  const type = this.req.url.split('/')[2]
+
   this.body = {
-    data: yield load.one('page', this.params.id)
-  }
-
-  yield next
-})
-
-router.get('/users', function *(next) {
-  this.body = {
-    data: yield load.all('user')
-  }
-
-  yield next
-})
-
-router.get('/users/:id', function *(next) {
-  this.body = {
-    data: yield load.one('user', this.params.id)
+    data: yield load.one(singular(type), this.params.id)
   }
 
   yield next
