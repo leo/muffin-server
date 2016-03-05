68946fa6cfe7255dfa42351b4f1cf9147887e085
Leo <leo@users.noreply.github.com>
2016-03-05 16:07:15 +0100

    Support for uploading files

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 const router = require('koa-router')()
 const jwt = require('koa-jwt')
+const fs = require('fs')
+const path = require('path')
 
 const log = require('../lib/log')
 const Page = require('../models/page')
 const User = require('../models/user')
 
 const load = require('../lib/get')
+const gfs = require('../lib/db').fs
 
 router.post('/token-auth', function *(next) {
   const body = this.request.body
@@ -106,6 +109,97 @@ router.post('/token-refresh', function *(next) {
   }
 
   yield next
+})
+
+function detSeparator (handle) {
+  var numbers = {}
+
+  const types = {
+    spaces: ' ',
+    dots: '.',
+    underscores: '_',
+    dashes: '-'
+  }
+
+  for (var type in types) {
+    if (!types.hasOwnProperty(type)) {
+      continue
+    }
+
+    numbers[type] = handle.split(types[type]).length - 1
+  }
+
+  const largest = Object.keys(numbers).reduce((prev, curr) => {
+    return numbers[prev] > numbers[curr] ? prev : curr
+  })
+
+  return types[largest]
+}
+
+router.post('/upload', function *(next) {
+  const file = this.request.body.files.file
+  const ext = path.extname(file.name)
+  const name = path.basename(file.name, ext)
+
+  // Determine the most used separator in the filename
+  const separator = detSeparator(name)
+
+  var id = 0
+
+  // Encode filename as URI string
+  function formatName (n) {
+    return encodeURIComponent(name + (n > 0 ? separator + n : '') + ext)
+  }
+
+  do {
+    // Check if filename is already in DB
+    try {
+      var isAvailable = yield gfs.exist({
+        filename: formatName(id),
+        root: 'media'
+      })
+    } catch (err) {
+      return log('Couldn\'t check if media exists', err)
+    }
+
+    // Increase id if it's already in use
+    if (isAvailable) {
+      id++
+    } else {
+      // If not, simply go on
+      break
+    }
+  } while (true)
+
+  const writestream = gfs.createWriteStream({
+    filename: formatName(id),
+    root: 'media',
+    content_type: file.type
+  })
+
+  // Get file from disk and pipe it to gfs's writestream
+  const content = fs.createReadStream(file.path)
+  content.pipe(writestream)
+
+  try {
+    yield new Promise(resolve => {
+      writestream.on('close', () => resolve())
+    })
+  } catch (err) {
+    return log('Not able to save media', err)
+  }
+
+  this.body = {
+    contentType: file.type,
+    uploadDate: new Date().toJSON()
+  }
+
+  yield next
+
+  // Remove temporary file after response
+  fs.unlink(file.path, function (err) {
+    if (err) log('Couldn\'t remove temporary media file', err)
+  })
 })
 
 function singular (type) {
