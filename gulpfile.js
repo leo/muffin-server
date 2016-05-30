93f0a10d423d74dc64c2dca11186ae8f40d1be01
Leonard Lamprecht <mindrun@icloud.com>
2016-05-30 21:11:46 +0200

    Put relevant directories on top level

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,18 @@ const babel = require('gulp-babel')
 const cache = require('gulp-cached')
 
 const paths = [
-  'lib/**/*',
+  'models/**/*',
+  'routes/**/*',
+  'utils/**/*',
+  'index.js'
 ]
 
+const config = {
+  base: '.'
+}
+
 gulp.task('transpile', () => {
-  return gulp.src(paths)
+  return gulp.src(paths, config)
   .pipe(cache('transpile'))
   .pipe(babel())
   .pipe(gulp.dest('dist'))
